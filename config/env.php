0179b751ab24af45a64e8742639d3c194b2e9429
mahabub96 <mahabubalam1035@gmail.com>
2026-01-12 19:01:47 +0600

    env.php

---
+++ config/env.php
@@ -0,0 +1,36 @@
+<?php 
+
+return [
+    //APP
+    'APP_NAME' => 'DEMO E-Commerce',
+    'APP_ENV' => 'local',
+    'APP_DEBUG' => true,
+    'APP_URL' => 'http://localhost:8000',
+
+
+    //Database
+    'DB_CONNECTION' => 'mysql',
+    'DB_HOST' => '127.0.0.1',
+    'DB_PORT' => '3306',
+    'DB_NAME' => 'ecommerce_db',
+    'DB_USER' => 'root',
+    'DB_PASS' => '',
+
+    //Mail / Notifications
+
+    'MAIL_HOST' => '',
+    'MAIL_PORT' => '',
+    'MAIL_USERNMAE' => '',
+    'MAIL_PASSWORD' => '',
+
+    //Payment Keys (for real keys)
+
+    'STRIPE_PUBLIC_KEY' => '',
+    'STRIPE_SECRET_KEY' => '',
+    'PAYPAL_CLIENT_ID' => '',
+    'PAYPAL_SECRET' => '',
+
+    //OTHERS
+    'APP_KEY' => '',
+
+];
